87ba03459d1bb80ce1a2f756200a2dac6eac9274
Luis-Sena-Av <luchito721@hotmail.com>
2023-05-30 17:36:18 -0500

    otra primera

--- src/components/Sobremi.jsx
+++ src/components/Sobremi.jsx
@@ -7,7 +7,7 @@ export const Sobremi = () => {
     <div className='sec2'>
         <div  className='sobremi hidden' id="sobremi">
         <b>¿ Quien soy ?</b>
-        <p>Soy un hombre encantado por el conocimiento sobre las matemáticas y algunos temas de la ingeniería. Nacido en el departamento de Córdoba/Colombia</p>
+        <p>Soy un hombre encantado por el conocimiento sobre las matemáticas, el desarrollo fronted  y algunos temas de la ingeniería. Nacido en el departamento de Córdoba/Colombia</p>
         <div className='img_presen'><img src="../imagenes/luis-s.png" alt="luis-seña" /></div>
         </div> 
     </div>
